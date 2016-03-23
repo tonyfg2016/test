67b6ea30c43e044bfd83c2aa630745c60d3feae8
Tony <tony.lui@foodgulu.com>
2016-03-23 13:36:00 +0800

    123

--- sw.js
+++ sw.js
@@ -17,3 +17,24 @@ self.addEventListener('push', function(event) {
       tag: 'my-tag'
     }));
 });
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://tonyfg2016.github.io/test/index.html';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
